5bb8e9d87fcb353c671aeca421592c66d8f3da7e
psyomn <psyomn@xenagoras.net>
2011-03-10 12:46:36 -0500

    New main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,27 @@
 /**
-*  \mainpage TinyStory
-*  \section Why TinyStory 
-*  This is a program which was developed in order for a friend to 
-*  write pretty stories in html. The idea is that the person writing
-*  the said story can simply run the program in the directory with 
-*  a specified text file, and then generate the story in a standalone
-*  html file, with interactivity provided with javascript.
-*  \section Why did reinvent some things you could've just included?
-*  Well, I was doing this project mainly for practice. So you'll see
-*  a datastructure or some other things I could have included from 
-*  other libraries...
-*  \section How much attention will you be giving this project?
-*  I'm going to complete it and have basic functionality, but 
-*  probably will not maintain it in the future, unless it acquires
-*  a reasonable 'big' userbase.
-*  \section Can I help?
-*  I would love to work with more people. Give me a shout at my email
-*  listed.
-*  @author Simon (psyomn) Symeonidis lethaljellybean@gmail.com
-*  @file
-*  @section
-*  This part is the main part of the program. It is responsible to 
-*  read the story file and pass the contents to the parser.
+* \mainpage TinyStory
+* \section a Why TinyStory 
+* This is a program which was developed in order for a friend to 
+* write pretty stories in html. The idea is that the person writing
+* the said story can simply run the program in the directory with 
+* a specified text file, and then generate the story in a standalone
+* html file, with interactivity provided with javascript.
+* \section b Why did reinvent some things you could've just included?
+* Well, I was doing this project mainly for practice. So you'll see
+* a datastructure or some other things I could have included from 
+* other libraries...
+* \section c How much attention will you be giving this project?
+* I'm going to complete it and have basic functionality, but 
+* probably will not maintain it in the future, unless it acquires
+* a reasonable 'big' userbase.
+* \section d Can I help?
+* I would love to work with more people. Give me a shout at my email
+* listed.
+* @author Simon (psyomn) Symeonidis lethaljellybean@gmail.com
+* @file
+* @section
+* This part is the main part of the program. It is responsible to 
+* read the story file and pass the contents to the parser.
 */
 
 // The special tags to take care of are >, ", #
