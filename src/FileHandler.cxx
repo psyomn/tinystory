2d3f7a58f8dd6e25208f8dd366d0eaf7b8a5da28
psyomn <lethaljellybean@gmail.com>
2012-05-13 23:06:23 -0400

    FileHandler.cxx: Aesthetic changes

--- src/FileHandler.cxx
+++ src/FileHandler.cxx
@@ -6,34 +6,40 @@
 /** Default constructor with no filename set. Default filename
 then is story.txt */
 FileHandler::FileHandler() : 
-  mFilename("story.txt"){}
+  mFilename("story.txt")
+{}
 
 /** \param fname the filename that will be opened. If name is
 not set, then story.txt will be used. */
 FileHandler::FileHandler(std::string fname="story.txt") :
-  mFilename(fname){}
+  mFilename(fname)
+{}
 
 /** Default constructor */
-FileHandler::~FileHandler(){
+FileHandler::~FileHandler()
+{
   mFilename.clear();
 }
 
 /** Read contents from a file. \param mFilename must be set */
-std::string FileHandler::read(){
+std::string FileHandler::read()
+{
   std::ifstream ifs;
   int fsize; // file size
   char* buff;
 
   ifs.open(mFilename.c_str());
 
-  if(ifs){
+  if(ifs)
+  {
     ifs.seekg(0,std::ios::end);
     fsize = ifs.tellg();
     ifs.seekg(0,std::ios::beg);
     buff = new char[fsize];
     ifs.read(buff,fsize);
     ifs.close();
-  } else {
+  } else 
+  {
     buff = (char*) "[FAIL]";
     std::cout << "Warning - failure opening file : " << mFilename << std::endl; 
   }
@@ -42,7 +48,8 @@ std::string FileHandler::read(){
 
 /** Function to write to a file. \param buff is the
 buffer to write out. */
-void FileHandler::write(std::string buff){
+void FileHandler::write(std::string buff)
+{
   std::ofstream ofs; 
   ofs.open(mFilename.c_str()); 
   ofs << buff; 
