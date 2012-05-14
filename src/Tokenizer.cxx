7ce5aa22ab493130fadd7061ed46648f34a59c51
psyomn <lethaljellybean@gmail.com>
2012-05-13 23:05:36 -0400

    Aesthetic changes

--- src/Tokenizer.cxx
+++ src/Tokenizer.cxx
@@ -4,30 +4,35 @@
 #include "Tokenizer.hxx"
 
 /** Default constructor */
-Tokenizer::Tokenizer(){}
+Tokenizer::Tokenizer()
+{}
 
 /** Default constructor */
-Tokenizer::~Tokenizer(){
+Tokenizer::~Tokenizer()
+{
   mTokens.clear();
 }
 
 /** Main tokenizing function. Does not store
 null tokens. \param str is a reference to the 
 string to tokenize */
-void Tokenizer::tokenize(std::string& str){
+void Tokenizer::tokenize(std::string& str)
+{
   std::stringstream ss; 
   std::string tmp;
 
   ss.str(str + "  ");
   
-  while(ss >> tmp){
+  while(ss >> tmp)
+  {
     mTokens.push_back(tmp);
   }
 }
 
 /** Function to tokenize the story \param fname 
 is the name of the story-file to read and tokenize */
-void Tokenizer::tokenizeStory(std::string fname){
+void Tokenizer::tokenizeStory(std::string fname)
+{
   std::string tmp;
   FileHandler fh(fname); 
   tmp = fh.read();
@@ -35,22 +40,26 @@ void Tokenizer::tokenizeStory(std::string fname){
 }
 
 /** Return the tokens read only for whatever reason */
-const std::vector<std::string> Tokenizer::getTokens() const {
+const std::vector<std::string> Tokenizer::getTokens() const 
+{
   return mTokens;
 }
 
 /** This prints the vector. This is a debugging routine.  */
-void Tokenizer::print() const {
+void Tokenizer::print() const 
+{
   std::vector<std::string>::const_iterator it;
 
-  for (it=mTokens.begin(); it!=mTokens.end(); ++it){
+  for (it=mTokens.begin(); it!=mTokens.end(); ++it)
+  {
     std::cout << *it << "."; 
   }
   std::cout << std::endl << &mTokens << std::endl;
 }
 
 /** Get a pointer to the vector containing the tokens */
-std::vector<std::string>* Tokenizer::getPointerToTokens() {
+std::vector<std::string>* Tokenizer::getPointerToTokens() 
+{
   return &mTokens;
 }
 
